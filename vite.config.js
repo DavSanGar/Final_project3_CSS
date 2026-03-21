8d5dd50ae3478a344adf30d79a149f6856229399
David Sanchez <dsg1989@protonmail.com>
2026-03-21 21:13:49 +0100

    incluir html secundario

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/Final_project3_CSS/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        location: resolve(__dirname, "location.html"),
+      },
+    },
+  },
 });
